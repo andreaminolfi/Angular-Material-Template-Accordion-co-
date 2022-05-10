f1b0289b9f006ee4a584dcd06435744fa30225a0
andreaminolfi <andrea@minolfi.it>
2022-05-10 11:47:06 +0200

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { delay } from 'rxjs/operators';
+import { User } from './model/user';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  constructor() {}
+  users: User[] | null = null;
+
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {
+    http
+      .get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .pipe(delay(1000))
+      .subscribe((res) => (this.users = res));
+  }
+
+  deleteHandler(id: number, buttonRef: MatButton) {
+    this.http
+      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .subscribe(() => {
+        if (this.users) {
+          this.users = this.users.filter((u) => u.id !== id);
+          this.snackBar.open('Item Removed', 'SUCCESS');
+        }
+      });
+  }
 }
